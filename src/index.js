92701bc73ec9ac2562ad12fe65a4f1b1797f2019
balin-sv <balinsvet@gmail.com>
2021-08-19 21:10:09 +0300

    feat: add all

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let aux = n.toString().replace(/\D/g, "");
+    let newString = "";
+    for (let i = aux.length - 1; i >= 0; i--) {
+        newString = newString + aux[i];
+    }
+    return parseInt(newString);
+};
